1000d795756752c6b3dffdbf90c09117f21ffc80
autom8ter <admin@autom8ter.com>
2019-05-09 14:59:50 -0600

    prototomsg

---
+++ pkg/functions/basic.go
@@ -0,0 +1,3 @@
+package functions
+
+type BasicFunc func()
--- pkg/pubsub/pubsub.go
+++ pkg/pubsub/pubsub.go
@@ -3,6 +3,7 @@
 package pubsub
 
 import (
+	"github.com/autom8ter/tasks/pkg/functions"
 	"github.com/golang/protobuf/ptypes/any"
 	"github.com/nats-io/nats"
 )
@@ -23,4 +24,16 @@ func NewNats(url string, opts ...nats.Option) (*Nats, error) {
 
 func (n *Nats) Publish(msg *any.Any) error {
 	return n.conn.Publish(msg.GetTypeUrl(), msg.GetValue())
+}
+
+func (n *Nats) SubscriptionBarrier(basicFunc functions.BasicFunc) error {
+	return n.conn.Barrier(basicFunc)
+}
+
+func ProtoToMsg(msg *any.Any, reply string) *nats.Msg {
+	return &nats.Msg{
+		Subject: msg.GetTypeUrl(),
+		Reply:   reply,
+		Data:    msg.GetValue(),
+	}
 }
